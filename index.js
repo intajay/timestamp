308a6529d0f213c93ae1241f482532d1f1a55f10
Ajay Meena <intajayk@gmail.com>
2017-01-19 11:25:42 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+
+var port = process.argv[2] || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:timestamp', function(req, res) {
+	var date;
+
+	if (isNaN(req.params.timestamp)) {
+		date = new Date(req.params.timestamp);
+	} else {
+		date = new Date(req.params.timestamp * 1000);
+	}
+
+	console.log(date);
+	var monthNames = ["January", "February", "March", "April", "May", "June", "July",
+	 "August", "September", "October", "November", "December"];
+
+	res.json({
+		unix: date.getTime() / 1000,
+		natural: monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+	});
+});
+
+app.listen(port, function() {
+	console.log('Server listening on Port ' + port);
+});
