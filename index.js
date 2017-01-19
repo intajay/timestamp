c73c445401c23652fe9a109d3d53382c06630bfc
Ajay Meena <intajayk@gmail.com>
2017-01-19 11:37:05 +0530

    Update port for heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-var port = process.argv[2] || 3000;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
